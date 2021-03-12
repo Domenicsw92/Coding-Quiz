f28d66d6e2544b53145cfd21e5dcf0d446509335
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-03-12 16:26:32 -0700

    js now shows a question trying to figure out how to select an answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 var gameStartBtn = document.getElementById('start-btn');
 var reStarteBtn = document.getElementById('restart-btn');
+var introText = document.getElementById('intro')
 var gameTimer = document.getElementById('timer');
 var gameTimerText = document.getElementById('timer-text');
 var questionCardElement = document.getElementById('question-card');
 var questionElement = document.getElementById('question');
 var answerBtnElemnt = document.getElementById('answer-btn');
+var feedBackElement = document.getElementById('feedback')
 var timerInterval = 1000;
-var timerCount = 0;
-var counter;
+var timerCount = 100;
+var timeCounter;
 
 
 
@@ -41,56 +43,77 @@ function gameStart() {
 
     gameStartBtn.addEventListener('click', function () {
         console.log(gameStartBtn)
-        counter = setInterval(function () {
-            timerCount--;
-            if (timerCount === 0) {
-                clearInterval(counter);
-                alert('Game Over')
-            }
-            updateTimer();
-        }, timerInterval)
         gameStartBtn.classList.add('hide')
+        introText.classList.add('hide')
         gameTimer.classList.remove('hide')
         gameTimerText.classList.remove('hide')
         shuffleQuestions = questions.sort(() => Math.random() - .5)
         currentQuestionIndex = 0
         questionCardElement.classList.remove('hide')
-        timerCount = 10;
         updateTimer();
         nextQuestion()
-
     })
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.choices.forEach( element => {
+    question.choices.forEach(element => {
         var button = document.createElement('button')
         console.log(button)
         button.classList.add('btn')
         button.innerText = element
         answerBtnElemnt.appendChild(button)
-        button.addEventListener('click', nextQuestion) 
+        button.addEventListener('click', nextQuestion)
     })
+}
+function resetQuestionCard() {
+    while (answerBtnElemnt.firstChild) {
+        answerBtnElemnt.removeChild(answerBtnElemnt.firstChild)
+    }
 }
 
 function nextQuestion() {
+    resetQuestionCard()
     showQuestion(shuffleQuestions[currentQuestionIndex])
 }
 
-
-
-
-
-
-
-function updateTimer() {
-    gameTimer.textContent = timerCount;
+function selectAnswers() {
+    if (this.value !== questions[currentQuestionIndex].choices) {
+        time -= 15;
+        if (time < 0) {
+            time = 0;
+        }
+        gameTimer.textContent = counter;
+        feedBackElement.textContent = "Wrong";
+    } else {
+        feedBackElement.textContent = "Correct";
+    }
+    feedBackElement.classList.remove('hide');
+    setTimeout(function () {
+        feedBackElement.classList.add('hide');
+    }, 1000);
+    currentQuestionIndex++;
 }
 
-function init() {
-    gameStart();
+
+
+
+timeCounter = setInterval(function () {
+    timerCount--;
+    if (timerCount === 0) {
+        clearInterval(timeCounter); 
+    }
     updateTimer();
-}
+}, timerInterval)
 
-init();
+
+    function updateTimer() {
+        gameTimer.textContent = timerCount;
+    }
+
+    function init() {
+        gameStart();
+        updateTimer();
+    }
+
+    init();
